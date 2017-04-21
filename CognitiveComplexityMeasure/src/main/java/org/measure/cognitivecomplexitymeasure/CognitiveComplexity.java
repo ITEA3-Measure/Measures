2d239ae22afecad7027c8e5273a736be92ed0b10
Antonin Abherve <antonin.abherve@softeam.fr>
2017-04-21 16:21:07 +0200

    Update on CogintiveComplexityMeasure

--- CognitiveComplexityMeasure/src/main/java/org/measure/cognitivecomplexitymeasure/CognitiveComplexity.java
+++ CognitiveComplexityMeasure/src/main/java/org/measure/cognitivecomplexitymeasure/CognitiveComplexity.java
@@ -134,7 +134,7 @@ public class CognitiveComplexity extends DirectMeasure{
     }
 
 
-/*
+
     public static void main(String[] args) {
 
 
@@ -164,6 +164,6 @@ public class CognitiveComplexity extends DirectMeasure{
         //System.out.println(destinationFolder);
 
 
-    }*/
+    }
 
 }
