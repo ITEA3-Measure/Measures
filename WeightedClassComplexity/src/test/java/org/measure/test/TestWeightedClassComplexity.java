f29526f3c9d1cce2ad4ca864efdd3dff98a3945c
Sarah Dahab <sasdahab@gmail.com>
2017-09-04 08:15:54 +0200

    first Weighted class complexity

---
+++ WeightedClassComplexity/src/test/java/org/measure/test/TestWeightedClassComplexity.java
@@ -0,0 +1,27 @@
+package org.measure.test;
+
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+import org.junit.Test;
+import org.measure.impl.WeightedClassComplexity;
+import org.measure.smm.measure.api.IMeasurement;
+
+@objid ("4b01b2ad-f0bd-4b88-aeed-c97022de6966")
+public class TestWeightedClassComplexity {
+    @objid ("c564728e-7816-463d-a72d-0d99de76a3f0")
+    @Test
+    public void testWeightedClassComplexity() {
+        WeightedClassComplexity measure = new WeightedClassComplexity();
+        measure.getProperties().put("URL", "https://svn.softeam.fr/svn/MEASURE/trunk/Software/SMM_EMF_API");
+        measure.getProperties().put("LOGIN","sdahab");
+        measure.getProperties().put("PASSWORD","3bI2RE78m&");
+
+        try {
+            for(IMeasurement result : measure.getMeasurement()){
+                System.out.println("Test Result : " + result.getLabel());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
