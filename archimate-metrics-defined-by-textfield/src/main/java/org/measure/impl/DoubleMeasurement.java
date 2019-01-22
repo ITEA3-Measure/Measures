01f42345104f8bbe0f8fbb7ce224c6ad93db0a7f
Kais CHAABOUNI <kais9chaabouni@gmail.com>
2019-01-22 16:04:48 +0100

    added archimate measures

---
+++ archimate-metrics-defined-by-textfield/src/main/java/org/measure/impl/DoubleMeasurement.java
@@ -0,0 +1,27 @@
+package org.measure.impl;
+
+import org.measure.smm.measure.defaultimpl.measurements.DefaultMeasurement;
+
+
+public class DoubleMeasurement extends DefaultMeasurement {
+
+    public DoubleMeasurement() {
+        
+    }
+
+    public void setValue(Double value) {
+        addValue("value",value);
+    }
+
+    public Double getValue() {
+        
+        return (Double) getValues().get("value");
+    }
+
+    @Override
+    public String getLabel() {
+        
+        return getValues().get("value").toString();
+    }
+
+}
