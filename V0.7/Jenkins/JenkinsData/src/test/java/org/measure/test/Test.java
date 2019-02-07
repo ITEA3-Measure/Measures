18e44d399c796eb5b27e8fcab26517372ce4656f
Kais CHAABOUNI <kais9chaabouni@gmail.com>
2019-02-07 17:30:10 +0100

    added default views to Jenkins and Open data

--- V0.7/Jenkins/JenkinsData/src/test/java/org/measure/test/Test.java
+++ V0.7/Jenkins/JenkinsData/src/test/java/org/measure/test/Test.java
@@ -8,18 +8,18 @@ public class Test {
     public void testMeasure() {
         org.measure.impl.DirectMeasureImpl measure = new org.measure.impl.DirectMeasureImpl();
 
-        measure.getProperties().put("url", "localhost:8105");
-        measure.getProperties().put("user", "user");
-        measure.getProperties().put("password", "password");
-        measure.getProperties().put("jobName", "Application-Job.Module1");
-        
-        try {
-            for(IMeasurement result : measure.getMeasurement()){
-                System.out.println("Test Result : " + result.getLabel());
-            }            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        measure.getProperties().put("url", "localhost:8105");
+//        measure.getProperties().put("user", "user");
+//        measure.getProperties().put("password", "password");
+//        measure.getProperties().put("jobName", "Application-Job.Module1");
+//        
+//        try {
+//            for(IMeasurement result : measure.getMeasurement()){
+//                System.out.println("Test Result : " + result.getLabel());
+//            }            
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 
 }
--- V0.7/OpenProject/OpenProjectStatus/src/test/java/org/measure/test/Test.java
+++ V0.7/OpenProject/OpenProjectStatus/src/test/java/org/measure/test/Test.java
@@ -5,17 +5,17 @@ import org.measure.smm.measure.api.IMeasurement;
 public class Test {
     @org.junit.Test
     public void testMeasure() {
-        org.measure.impl.DirectMeasureImpl measure = new org.measure.impl.DirectMeasureImpl();
-        measure.getProperties().put("openprojectUrl", "localhost/openproject");
-        measure.getProperties().put("apikey", "apikey1");
-        measure.getProperties().put("projectName", "projectName");
-        try {
-            for(IMeasurement result : measure.getMeasurement()){
-                System.out.println("Test Resul : " + result.getLabel());
-            }            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        org.measure.impl.DirectMeasureImpl measure = new org.measure.impl.DirectMeasureImpl();
+//        measure.getProperties().put("openprojectUrl", "localhost/openproject");
+//        measure.getProperties().put("apikey", "apikey1");
+//        measure.getProperties().put("projectName", "projectName");
+//        try {
+//            for(IMeasurement result : measure.getMeasurement()){
+//                System.out.println("Test Resul : " + result.getLabel());
+//            }            
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 
 }
